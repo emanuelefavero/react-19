a21844358c99b6e5885bf3ffeddfe39c1d721774
emanuelefavero <emafaux89@gmail.com>
2024-05-28 23:17:23 +0200

    add actions

---
+++ app/actions/page.js
@@ -0,0 +1,50 @@
+'use client'
+
+import React, { useState, useCallback } from 'react'
+
+async function submitForm() {
+  // Simulate a server action
+  // TIP: To use a server action in a client component, you need to put the action in a separate file (e.g. app/actions.js) and add the 'use server' flag at the top of the file
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({ message: 'Action completed' })
+    }, 1000)
+  })
+}
+
+export default function Page() {
+  const [formState, setFormState] = useState(null)
+  const [isPending, setIsPending] = useState(false)
+
+  const formAction = useCallback(async (event) => {
+    event.preventDefault()
+    setIsPending(true)
+
+    try {
+      const result = await submitForm()
+      setFormState(result)
+    } catch (error) {
+      setFormState({ message: 'Failed to complete action' })
+    }
+
+    setIsPending(false)
+  }, [])
+
+  return (
+    <>
+      <h1>Actions</h1>
+
+      <form onSubmit={formAction}>
+        <button
+          type='submit'
+          disabled={isPending}
+          className={isPending && 'bg-slate-500 hover:bg-slate-500'}
+        >
+          {isPending ? 'Loading...' : 'Submit'}
+        </button>
+      </form>
+
+      {formState && <p>{formState.message}</p>}
+    </>
+  )
+}
--- app/page.js
+++ app/page.js
@@ -1,7 +1,17 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <main>
       <h1>React 19</h1>
+
+      <nav>
+        <ul>
+          <li>
+            <Link href='/actions'>Actions</Link>
+          </li>
+        </ul>
+      </nav>
     </main>
   )
 }
