4215827bb2603694ca25d2a5d10d1fe625b27850
emanuelefavero <emafaux89@gmail.com>
2024-05-30 18:24:47 +0200

    add useState to client-form-submission

--- app/client-form-submission/page.js
+++ app/client-form-submission/page.js
@@ -1,10 +1,15 @@
 'use client'
 
+import { useState } from 'react'
+
 export default function Page() {
+  const [name, setName] = useState('')
+
   // TIP: greet() is a client side "action"
   function greet(formData) {
     let name = formData.get('name')
     alert('Hello ' + name)
+    setName(name)
   }
 
   return (
@@ -12,9 +17,11 @@ export default function Page() {
       <h1>Client Form Submission</h1>
 
       <form action={greet}>
-        <input type='text' name='name' />
+        <input type='text' name='name' placeholder='Name' />
         <button type='submit'>Search</button>
       </form>
+
+      {name && <p>Hello {name}</p>}
     </>
   )
 }
