794e970331c914d12b9c68ceea78ed96ae09a89d
emanuelefavero <emafaux89@gmail.com>
2024-05-31 21:46:04 +0200

    move use-optimistic example to separate component

---
+++ app/use-optimistic/Example1.js
@@ -0,0 +1,65 @@
+import { useOptimistic, useState, useRef } from 'react'
+import { deliverMessage } from '@/app/actions.js'
+
+function Messages({ messages, sendMessageToServer }) {
+  const formRef = useRef()
+
+  const action = async (formData) => {
+    // Optimistically add the message to the UI
+    addOptimisticMessage(formData.get('message'))
+    formRef.current.reset() // clear the form
+
+    // Send the message to the server
+    await sendMessageToServer(formData)
+  }
+
+  const [optimisticMessages, addOptimisticMessage] = useOptimistic(
+    messages, // initial state that will be passed to optimisticMessages
+    (prevState, newMessage) => [
+      ...prevState,
+      {
+        text: newMessage, // newMessage = formData.get('message')
+        sending: true, // this will be set to false after the server responds
+      },
+    ]
+  )
+
+  return (
+    <>
+      {optimisticMessages.map((message, index) => (
+        <div key={index}>
+          {message.text}
+          {!!message.sending && <small> (Sending...)</small>}
+        </div>
+      ))}
+
+      <form action={action} ref={formRef} className='mt-2'>
+        <input
+          type='text'
+          name='message'
+          placeholder='Message...'
+          className='mr-2'
+        />
+        <button type='submit'>Send Message</button>
+      </form>
+    </>
+  )
+}
+
+export default function Component() {
+  const [messages, setMessages] = useState([{ text: 'Hello', sending: false }])
+
+  // Call server action and update the UI with the response
+  const sendMessageToServer = async (formData) => {
+    const sentMessage = await deliverMessage(formData.get('message'))
+    setMessages((messages) => [...messages, { text: sentMessage }])
+  }
+
+  return (
+    <>
+      <Messages messages={messages} sendMessageToServer={sendMessageToServer} />
+    </>
+  )
+}
+
+/* TIP: The !! operator converts any value to a boolean. It is used here to make sure the sending value always outputs true or false even if other values are assigned to it */
--- app/use-optimistic/page.js
+++ app/use-optimistic/page.js
@@ -1,57 +1,8 @@
 'use client'
 
-import { useOptimistic, useState, useRef } from 'react'
-import { deliverMessage } from '@/app/actions.js'
-
-function Messages({ messages, sendMessageToServer }) {
-  const formRef = useRef()
-
-  const action = async (formData) => {
-    // Optimistically add the message to the UI
-    addOptimisticMessage(formData.get('message'))
-    formRef.current.reset() // clear the form
-
-    // Send the message to the server
-    await sendMessageToServer(formData)
-  }
-
-  const [optimisticMessages, addOptimisticMessage] = useOptimistic(
-    messages, // initial state that will be passed to optimisticMessages
-    (prevState, newMessage) => [
-      ...prevState,
-      {
-        text: newMessage, // newMessage = formData.get('message')
-        sending: true, // this will be set to false after the server responds
-      },
-    ]
-  )
-
-  return (
-    <>
-      {optimisticMessages.map((message, index) => (
-        <div key={index}>
-          {message.text}
-          {!!message.sending && <small> (Sending...)</small>}
-        </div>
-      ))}
-
-      <form action={action} ref={formRef}>
-        <input type='text' name='message' placeholder='Message...' />
-        <button type='submit'>Send Message</button>
-      </form>
-    </>
-  )
-}
+import Example1 from './Example1.js'
 
 export default function Page() {
-  const [messages, setMessages] = useState([{ text: 'Hello', sending: false }])
-
-  // Call server action and update the UI with the response
-  const sendMessageToServer = async (formData) => {
-    const sentMessage = await deliverMessage(formData.get('message'))
-    setMessages((messages) => [...messages, { text: sentMessage }])
-  }
-
   return (
     <>
       <h1>useOptimistic</h1>
@@ -61,9 +12,7 @@ export default function Page() {
         optimistically while waiting for the server response.
       </p>
 
-      <Messages messages={messages} sendMessageToServer={sendMessageToServer} />
+      <Example1 />
     </>
   )
 }
-
-/* TIP: The !! operator converts any value to a boolean. It is used here to make sure the sending value always outputs true or false even if other values are assigned to it */
