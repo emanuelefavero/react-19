ddeac286adf5ed61028a244042e26f6fdac97d1c
emanuelefavero <emafaux89@gmail.com>
2024-06-04 18:46:08 +0200

    add loading and error to custom hook

--- app/custom-hooks/page.js
+++ app/custom-hooks/page.js
@@ -3,13 +3,22 @@
 import { useFetch } from '@/hooks/useFetch'
 
 export default function Page() {
-  const data = useFetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
+  const {
+    data: posts,
+    loading: loadingPosts,
+    error: postsError,
+  } = useFetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
+
+  if (postsError) console.error(postsError)
 
   return (
     <>
       <h1>Custom Hooks</h1>
 
-      <ul>{data && data.map((post) => <li key={post.id}>{post.title}</li>)}</ul>
+      <ul>
+        {posts && posts.map((post) => <li key={post.id}>{post.title}</li>)}
+        {loadingPosts && <p>Loading...</p>}
+      </ul>
     </>
   )
 }
--- hooks/useFetch.js
+++ hooks/useFetch.js
@@ -4,16 +4,21 @@ import { useState, useEffect } from 'react'
 
 export function useFetch(url) {
   const [data, setData] = useState()
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
 
   useEffect(() => {
+    setLoading(true)
+
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
         setData(data)
-        setLoading(true)
+        setLoading(false)
       })
-      .catch((error) => console.error(error))
-  }, [])
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false))
+  }, [url])
 
-  return data
+  return { data, loading, error }
 }
