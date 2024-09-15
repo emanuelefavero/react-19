f1c399f7a06433d116f941ef90fa30c311604ebf
emanuelefavero <emafaux89@gmail.com>
2024-09-15 05:31:38 +0200

    update contex-as-provider

--- app/context-as-provider/page.js
+++ app/context-as-provider/page.js
@@ -7,7 +7,9 @@ export default function Page() {
 
       <p className='mb-2'>
         There's no need for <code>{'<Context.Provider>'}</code> anymore. You can
-        use <code>{'<Context>'}</code> directly instead.
+        use <code>{'<Context>'}</code> directly instead. Also, we now can
+        utilize <code>use</code> instead of <code>useContext</code> when getting
+        context values.
       </p>
 
       <p>
@@ -16,7 +18,7 @@ export default function Page() {
           {'@/app/context/ThemeContext'} and{' '}
           <code>{'@/app/use/ContextExample'}</code>{' '}
         </code>
-        in this project codebase for an example.
+        in this project codebase for examples.
       </p>
     </>
   )
