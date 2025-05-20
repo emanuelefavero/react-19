5ff38e253aacf5d10e30cc36d882e1eb58595ca1
Emanuele Favero <emafaux89@gmail.com>
2025-05-20 06:25:16 +0200

    feat: use-optimistic example 3 clear form before action

--- app/use-optimistic/Example3.js
+++ app/use-optimistic/Example3.js
@@ -1,9 +1,10 @@
-import { useOptimistic, useState } from 'react'
+import { useOptimistic, useState, useRef } from 'react'
 import { addTodo } from '@/app/actions.js'
 import { todos as initialTodos } from '@/data/todos.js'
 import { v4 as uuidv4 } from 'uuid'
 
 export default function Component() {
+  const formRef = useRef(null)
   const [todos, setTodos] = useState(initialTodos)
   const [optimisticTodos, setOptimisticTodos] = useOptimistic(todos)
 
@@ -18,6 +19,8 @@ export default function Component() {
     // Optimistically add the todo to the UI
     setOptimisticTodos((prevState) => [...prevState, newTodo])
 
+    formRef.current.reset() // clear the form before sending the request
+
     // Send the same todo object to the server
     const updatedTodos = await addTodo(newTodo)
     setTodos(updatedTodos) // update the state with the server response
@@ -27,13 +30,7 @@ export default function Component() {
     <>
       <h2>Example 3</h2>
 
-      <ul className='mb-2'>
-        {optimisticTodos.map((todo) => (
-          <li key={todo.id}>{todo.text}</li>
-        ))}
-      </ul>
-
-      <form action={action}>
+      <form action={action} ref={formRef}>
         <input
           type='text'
           name='todo'
@@ -43,6 +40,12 @@ export default function Component() {
         />
         <button type='submit'>Add Todo</button>
       </form>
+
+      <ul className='mb-2'>
+        {optimisticTodos.map((todo) => (
+          <li key={todo.id}>{todo.text}</li>
+        ))}
+      </ul>
     </>
   )
 }
