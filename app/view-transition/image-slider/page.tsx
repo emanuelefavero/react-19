3f6573144af7658507141ffba5accbf081ad003a
Emanuele Favero <emafaux89@gmail.com>
2025-11-04 15:45:39 +0100

    refactor: move view-transition image silder in sub route

---
+++ app/view-transition/image-slider/page.tsx
@@ -0,0 +1,10 @@
+import ImageSlider from './ImageSlider'
+
+export default function Page() {
+  return (
+    <>
+      <h1 className='mt-4'>View Transition Image Slider</h1>
+      <ImageSlider />
+    </>
+  )
+}
--- app/view-transition/page.tsx
+++ app/view-transition/page.tsx
@@ -1,10 +1,15 @@
-import ImageSlider from './ImageSlider'
+import Link from 'next/link'
 
 export default function Page() {
   return (
     <>
-      <h1 className='mt-4'>View Transition Image Slider</h1>
-      <ImageSlider />
+      <h1 className='mt-4'>View Transition</h1>
+
+      <ul className='mt-2'>
+        <li>
+          <Link href='/view-transition/image-slider'>Image Slider</Link>
+        </li>
+      </ul>
     </>
   )
 }
