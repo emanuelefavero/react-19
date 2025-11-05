5d92b95a8503512a91314128ace8584547f5dad1
Emanuele Favero <emafaux89@gmail.com>
2025-11-05 21:24:52 +0100

    build: add bundle analyzer

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from 'next'
+import withBundleAnalyzer from '@next/bundle-analyzer'
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
@@ -9,4 +10,6 @@ const nextConfig: NextConfig = {
   },
 }
 
-export default nextConfig
+export default withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})(nextConfig)
